d77f27395f80932bc7a0315a147b72740f53e385
IntellegentAlie <104872961+IntellegentAlien@users.noreply.github.com>
2022-05-04 00:17:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import random
+
+# name = input('What is your name?\n')
+# print('Hi, %s.' % name)
+# age = int(input('Whay is your age? \n'))
+# print("You are %i years old." % age)
+
+# x = 10
+# print(x)
+
+# num = int(input('What is your number?'))
+
+# if num == 10:
+#   print(str(num) + ' is 10')
+# elif num > 10:
+#   print(str(num) + ' is more than 10')
+# else:
+#   print(str(num) + ' is less than 10')
+
+# num2 = int(input("What is your number:  "))
+# if num2 % 2 == 0:
+#   print(str(num2) + " is even")
+# else:
+#   print(str(num2) + " is odd")
+
+# list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+# for n in range(len(list)):
+#   print(list[n])
+
+# num_list = []
+# num3 = int(input("What is your number:  "))
+# count = 0
+# for n in range(10):
+#   num_list.append(random.randint(1, 100))
+# print(num_list)
+
+
+# sum = 0
+# num_list2 = []
+# while True:
+#   num_4 = input("What is your number (enterdone to quit) ")
+#   if num_4 == 'stop':
+#     break
+#   else:
+#     num_list2.append((int(num_4)))
+
+# average = 0
+# for n in range(len(num_list2)):
+#   average += num_list2[n]
+# average /= len(num_list2)
+# print(average)
+
+while True:
+  word = input('What is your word')
+  for n in range len(l)
